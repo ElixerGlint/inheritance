aeec564663a810c9e585bcd37fdee4100df18997
Elixer <everett.rumery@gmail.com>
2025-01-27 13:39:51 -0500

    first commit

---
+++ src/corgi.java
@@ -0,0 +1,27 @@
+public class corgi extends Dog {
+    private double legsize;
+    public corgi() {
+        this("Cheddar",0,4);
+    }
+
+    public corgi (String name, int age, double leg) {
+        super(name, age);
+        legsize = leg;
+    }
+
+    @Override
+    public String speak() {
+        // TODO Auto-generated method stub
+        return "Cute Barkaloooooo";
+    }
+
+    //overload
+    public String speak(int count) { //same name but diffrrent parameters
+        String output = "";
+        for(int i = 0; i < count; i++) {
+            output += this.speak();
+        }
+        return output;
+    }
+    
+}
